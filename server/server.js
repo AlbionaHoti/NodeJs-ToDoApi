5c8089353f24335bfe2962a4d28993263fd100c2
hoti <albiona@instaffo.de>
2018-09-19 16:35:13 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -31,6 +32,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+    res.send({todo});
+    }).catch((e) => {
+      res.status(400).send()
+    });
 });
 
 app.listen(3001, () => {
