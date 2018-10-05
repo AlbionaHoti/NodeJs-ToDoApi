c00d98f87b2692ca0770fc5569fcacbe3be25124
hoti <albiona@instaffo.de>
2018-10-05 13:48:53 +0200

    fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+  useNewUrlParser: true }
+);
 
 module.exports = {
   mongoose
